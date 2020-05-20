bb09445b7c2a263267d8cc7d6f04ec0f7b2bba62
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-05-20 20:33:32 +0200

    Provide image name in the form [owner]/[repo]/[registry]

--- .github/workflows/variables.sh
+++ .github/workflows/variables.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-SHA8=$(echo ${GIT_SHA} | cut -c1-8)
-echo "SHA8: ${SHA8}"
-echo "::set-output name=sha8::${SHA8}"
+sha8=$(echo ${GIT_SHA} | cut -c1-8)
+github_repo_name="${GITHUB_REPO#"$GITHUB_OWNER"/}"
+image_name="${GITHUB_REPO}/${github_repo_name}"
+
+echo "Repo name: ${github_repo_name}"
+echo "Image name: ${image_name}"
+echo "SHA8: ${sha8}"
+
+echo "::set-output name=sha8::${sha8}"
+echo "::set-output name=image_name::${image_name}"
